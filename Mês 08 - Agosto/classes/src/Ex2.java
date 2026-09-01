87ca4d3130153e97d8205706413073df04b1c281
cauansilva1423-debug <cauansilva1423@gmail.com>
2026-09-01 19:42:12 -0300

    adicionarex2

---
+++ Mês 08 - Agosto/classes/src/Ex2.java
@@ -0,0 +1,34 @@
+public class Ex2 {
+
+    public static void main(String[] args) {
+        Mercado unidadeJoinville = new Mercado();
+
+      
+        unidadeJoinville.nomeMercado = "Giassi";
+        unidadeJoinville.macasVendidas = 700;
+        unidadeJoinville.precoMaca = 2;
+        unidadeJoinville.laranjasVendidas = 100;
+        unidadeJoinville.precoLaranja = 2.5;
+
+
+        Mercado unidadeBlumenau = new Mercado();
+
+
+        unidadeBlumenau.nomeMercado = "Angeloni";
+        unidadeBlumenau.macasVendidas = 250;
+        unidadeBlumenau.precoMaca = 2;
+        unidadeBlumenau.laranjasVendidas = 320;
+        unidadeBlumenau.precoLaranja = 2.7;
+
+        Mercado unidadeFlorianopolis = new Mercado();
+
+        unidadeFlorianopolis.nomeMercado = "Fort";
+        unidadeFlorianopolis.macasVendidas = 350;
+        unidadeFlorianopolis.precoMaca = 3;
+        unidadeFlorianopolis.laranjasVendidas = 520;
+        unidadeFlorianopolis.precoLaranja = 2.5;
+
+        System.out.println(unidadeBlumenau.nomeMercado + " vendeu " + unidadeBlumenau.macasVendidas + " maçãs a R$"
+                + unidadeBlumenau.precoMaca);
+    }
+}
---
+++ Mês 08 - Agosto/classes/src/Mercado.java
@@ -0,0 +1,14 @@
+public class Mercado {
+
+    String nomeMercado;
+
+
+    int macasVendidas;
+
+    double precoMaca;
+
+    int laranjasVendidas;
+
+    double precoLaranja;
+
+}
